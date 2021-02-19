03a6b19f6b5ef4908a3e21331d253cbdf2613dd3
jinook929 <jinook929@gmail.com>
2021-02-19 17:54:21 -0500

    commit tree 898d754f3edac4a561e445ffaa96dd9330f96af9

--- app.rb
+++ app.rb
@@ -1,4 +1,8 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get('/') {"Hello, World!"}
+  get('/name') {"My name is Jinook."}
+  get('/hometown') {"My hometown is Seoul."}
+  get('/favorite-song') {"My favorite song is 'Yet Not I But through Christ in Me'."}
 end
